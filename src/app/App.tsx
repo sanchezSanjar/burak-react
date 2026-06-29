ac41441f46de67479364f7cc84a81a3896cde24c
sanchezSanjar <sanjar@ajou.ac.kr>
2026-06-29 13:26:21 +0900

    feat: develop header and footer part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,18 @@
 // import React from "react";
-import "../css/App.css";
 // import { Box, Button, Container, Stack, Typography } from "@mui/material";
+
 import {Link, Route, Switch, useLocation} from "react-router-dom";
 import { HomePage } from "./screens/homePage";
 import { ProductsPage } from "./screens/productsPage";
 import { OrdersPage } from "./screens/ordersPage";
 import { UserPage } from "./screens/userPage";
+import { HelpPage } from "./screens/helpPage";
 import { HomeNavbar } from "./components/headers/HomeNavbar";
 import { OtherNavbar } from "./components/headers/OtherNavbar";
 import { Footer } from "./components/footer";
+import "../css/App.css";
+import "../css/navbar.css";
+
 
 function App() {
  const location = useLocation();
@@ -26,6 +30,9 @@ function App() {
           </Route>
           <Route path="/member-page">
             <UserPage/>
+          </Route>
+          <Route path="/help ">
+            <HelpPage/>
           </Route>
           <Route path="/">
             <HomePage />
