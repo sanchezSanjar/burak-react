20911bdb253502d9cb88a1dd3df60f4b32cad502
sanchezSanjar <sanjar@ajou.ac.kr>
2026-07-01 17:17:21 +0900

    fix:organize homepage related sectional components

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,5 +1,22 @@
-import { Container } from "@mui/material";
+import React from "react";
+import ActiveUsers from "./ActiveUsers";
+import Advertisement from "./Advertisement";
+import Events from "./Events";
+import NewDishes from "./NewDishes";
+import PopularDishes from "./PopularDishes";
+import Statistics from "./Satistics";
+
+import "../../../css/home.css"
 
 export default function HomePage() {
-    return <Container>HomePage</Container>
+  return (
+    <div className={"homepage"}>
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
 }
