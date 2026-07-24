58b56cf503f3989c1a8c3948a6e58d9db3abbd23
sanchezSanjar <sanjar@ajou.ac.kr>
2026-07-24 14:46:58 +0900

    feat: create config & env variables

---
+++ src/lib/config.ts
@@ -0,0 +1,10 @@
+export const serviceApi: string = `${process.env.REACT_APP_API_URL}`;
+
+
+export const Messages = {
+    error1: "Something went wrong!",
+    error2: "Please login first!",
+    error3: "Please fullfill all unputs!",
+    error4: "Message is empty!",
+    error5: "Only images with jpeg, jpg, png format allowed!",
+};
