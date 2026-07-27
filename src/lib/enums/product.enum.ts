c4aee1d9520292dcd690f459f8265aa99bf24ad7
sanchezSanjar <sanjar@ajou.ac.kr>
2026-07-27 11:49:03 +0900

    feat: develop Product component logic

--- src/lib/enums/product.enum.ts
+++ src/lib/enums/product.enum.ts
@@ -23,7 +23,7 @@ export enum ProductStatus {
 export enum ProductCollection {
     DISH = "DISH",
     SALAD = "SALAD",
-    DESERT = "DESERT", 
+    DESSERT = "DESSERT", 
     DRINK = "DRINK",
     OTHER = "OTHER",
 }
